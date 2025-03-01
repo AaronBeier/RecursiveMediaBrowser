94d0b6cd1f518b3efb29da250bcf4409f36ce522
Aaron Beier <aaronbeier@proton.me>
2025-03-01 22:49:46 +0100

    Replace DllImport with LibraryImport

--- NaturalStringComparer.cs
+++ NaturalStringComparer.cs
@@ -2,15 +2,15 @@
 
 namespace RecursiveMediaBrowser {
 
-  internal sealed class NaturalStringComparer : IComparer<string> {
+  internal sealed partial class NaturalStringComparer : IComparer<string> {
 
     // This code is from https://stackoverflow.com/questions/248603/natural-sort-order-in-c-sharp
     // StrCmpLogicalW Reference:
     // https://learn.microsoft.com/en-us/windows/win32/api/shlwapi/nf-shlwapi-strcmplogicalw
     // https://www.pinvoke.net/default.aspx/shlwapi/StrCmpLogicalW.html
 
-    [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
-    public static extern int StrCmpLogicalW(string psz1, string psz2);
+    [LibraryImport("shlwapi.dll", EntryPoint = "StrCmpLogicalW", StringMarshalling = StringMarshalling.Utf16)]
+    public static partial int StrCmpLogicalW(string psz1, string psz2);
 
     public int Compare(string? A, string? B) {
       return StrCmpLogicalW(A ?? string.Empty, B ?? string.Empty);
